028593f7d0ae101a62e734fe0a2130f08e5940ae
Jairo Alonso Fernandez Vega <jairo.fernandez@youse.com.co>
2018-11-22 16:54:20 -0500

    form article

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'hello/index'
+
+  resources :articles
+  
+  root 'welcome#index'
 end
