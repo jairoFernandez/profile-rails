be85e6dcc12150c976966de8672b5f709f1fd7cd
Jairo Alonso Fernandez Vega <jairo.fernandez@youse.com.co>
2018-11-23 09:28:01 -0500

    Add new controllers

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # COMMANDS
 
+rails db:create
+rails generate migration CreateMyModel >> Generate a sentence for table
+rails generate migration MyModel >> Generate a sentence for table
+rails generate migration TypeArticle name:string description:string
 rails db:migrate
+
+rails db:rollback VERSION=2342342343
+rails dbconsole // .tables
+
+rails generate models Shoes
+
+rails console
+
 heroku run rake db:migrate
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_212257) do
+ActiveRecord::Schema.define(version: 2018_11_22_231433) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
   end
 
 end
